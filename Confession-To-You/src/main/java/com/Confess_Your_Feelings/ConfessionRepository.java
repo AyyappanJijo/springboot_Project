ee23e111c96b1e5f77fa69f8fad43ebaa6c2f3df
Ayyappan Jijo <ayyappanjijo2002@gmail.com>
2026-03-20 18:10:50 +0530

    Delete Confession-To-You directory

--- Confession-To-You/src/main/java/com/Confess_Your_Feelings/ConfessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Confess_Your_Feelings;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfessionRepository extends JpaRepository<Confession, Long> {
-
-}
